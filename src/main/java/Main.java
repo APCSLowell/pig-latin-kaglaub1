0d391cf7527d29e6fd285fd09941e941b9bfe4ec
kaglaub1 <kaglaub@s.sfusd.edu>
2025-01-24 12:14:25 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,25 @@
-
-
 public class Main {
 	
 	public static void main(final String[] args) {
 		PigLatin sim = new PigLatin();
-        	//sim.tester();
+		String translate = new String("");
+        	String consonants = new String("bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ");
+		String vowels = new String("aeiouAEIOU");
+		if (vowels.contains(args.substring(0, 1)) == true){
+			translate = translate + args + "way";
+		}
+		if (args.substring(0, 2).equals("qu") == true){
+			translate = translate + args.substring(2) + "ay";
+		}
+		boolean e = true;
+		for (int i = 0; i < args.length(); i++){
+			if (consonants.contains(args.substring(i, i+1)) == false){
+				e = false;
+			}
+		}
+		if (e == true){
+			translate = translate + args + "ay";
+		}
 	}
 	
 
