1bfb5f2cd30951fa323c3a906f8c53f5b975b008
kaglaub1 <kaglaub@s.sfusd.edu>
2025-01-27 11:54:03 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,15 @@ public class Main {
 		String translate = new String("");
         	String consonants = new String("bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ");
 		String vowels = new String("aeiouAEIOU");
+		//start with vowel
 		if (vowels.contains(args.substring(0, 1)) == true){
 			translate = translate + args + "way";
 		}
+		//start with qu
 		if (args.substring(0, 2).equals("qu") == true){
 			translate = translate + args.substring(2) + "ay";
 		}
+		//no vowels
 		boolean e = true;
 		for (int i = 0; i < args.length(); i++){
 			if (consonants.contains(args.substring(i, i+1)) == false){
